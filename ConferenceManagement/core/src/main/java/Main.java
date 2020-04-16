264f50fd172f005e9ccecb421197a27079861886
Cristi <cristian.ciont@gmail.com>
2020-04-16 13:14:03 +0300

    ORM Hibernate Implementation Works

--- ConferenceManagement/core/src/main/java/Main.java
+++ ConferenceManagement/core/src/main/java/Main.java
@@ -1,5 +1,18 @@
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import ui.Console;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("Cal");
+
+        System.out.println("hello");
+
+        AnnotationConfigApplicationContext context =
+                new AnnotationConfigApplicationContext(
+                        "config"
+                );
+
+        Console console = context.getBean(Console.class);
+
+        System.out.println("bye");
     }
 }
