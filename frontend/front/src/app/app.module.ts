6554f3a4de71872f0a72e8155366db5f877d1308
condoran <anco990@gmail.com>
2020-05-16 20:28:53 +0300

    pages

--- frontend/front/src/app/app.module.ts
+++ frontend/front/src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
 import { MainPageComponent } from './main-page/main-page.component';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { RegisterPageComponent } from './register-page/register-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
-    MainPageComponent
+    MainPageComponent,
+    LoginPageComponent,
+    RegisterPageComponent
   ],
   imports: [
     BrowserModule,
