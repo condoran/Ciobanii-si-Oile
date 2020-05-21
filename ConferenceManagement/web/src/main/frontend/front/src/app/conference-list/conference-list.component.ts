fa57d04d8654fa4a70c721f8d0b7b208703550d1
Ciobanu Valentin Eduard <valentin.ciobanu98@gmail.com>
2020-05-21 13:52:12 +0300

    cAL

---
+++ ConferenceManagement/web/src/main/frontend/front/src/app/conference-list/conference-list.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import {ConferenceService} from '../shared/conference.service';
+import {Conference} from '../shared/conference.model';
+
+@Component({
+  selector: 'app-conference-list',
+  templateUrl: './conference-list.component.html',
+  styleUrls: ['./conference-list.component.css']
+})
+export class ConferenceListComponent implements OnInit {
+  private conferences: Conference[];
+  constructor(private conferenceService: ConferenceService) { }
+
+  ngOnInit(): void {
+    this.conferenceService.getConferences()
+      .subscribe(conferences => this.conferences = conferences);
+  }
+
+}
